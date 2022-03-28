192a7fbb669bef67534f2784cb9da09bbf08e8e0
Natanael Medrado <natanaelmedrado@outlook.com>
2022-03-27 22:24:37 -0300

    Atualização Repositório

--- Orientacao a Objetos/colaboradores/CalculaBonificacao.java
+++
@@ -1,9 +0,0 @@
-/*
-Interface usada para criação de um método para ser aplicado à duas classes distintas (Polimorfismo)
- */
-
-package aulas.oo.colaboradores;
-
-public interface CalculaBonificacao {
-    void calculaBonificacao(double porcentagemBonificacao);
-}
